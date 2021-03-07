1d9c21b087e56b2a309a7f3dd31c7aada8220823
clement <clement.come@student-cs.fr>
2021-03-07 18:58:52 +0100

    Use annotation for prediction

--- src/main.py
+++ src/main.py
@@ -34,6 +34,7 @@ train_dataset = LymphocytosisDataset(
     fill_img_list=True,
     split_label=True,
     convert_age=True,
+    convert_gender=True,
 )
 print(train_dataset.__len__())
 
@@ -49,6 +50,7 @@ val_dataset = LymphocytosisDataset(
     fill_img_list=True,
     split_label=True,
     convert_age=True,
+    convert_gender=True,
 )
 
 print(val_dataset.__len__())
@@ -68,41 +70,37 @@ optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.0001)
 
 def train():
     model.train()
-    
-    conf_matrix = np.zeros((2,2))
+
+    conf_matrix = np.zeros((2, 2))
     # nbr_batches = int(train_loader.__len__() / batch_size) + 1
     for i, (annotation, bag, targets) in enumerate(train_loader):
-        #print("in batch {}".format(i))
+        # print("in batch {}".format(i))
         bag = bag.permute(0, 2, 1, 3, 4)
         targets = torch.stack(targets).T
-        #print(targets.shape)
+        # print(targets.shape)
         if use_cuda:
             bag, targets = bag.cuda(), targets.cuda()
 
         optimizer.zero_grad()
-        output = model(bag)
+        output = model(bag, annotation)
 
         # pred = output.data.max(1, keepdim=True, dtype = np.float32)[1]
 
-      
-
         loss = torch.nn.functional.binary_cross_entropy(output, targets.float())
- 
+
         loss.backward()
         optimizer.step()
         output = output.detach().cpu().numpy()
         targets = targets.detach().cpu().numpy()
-        conf_matrix+=metrics.confusion_matrix(targets, output>0.5)
+        conf_matrix += metrics.confusion_matrix(targets, output > 0.5)
 
     return conf_matrix
-
-
 
 
 def validation():
     model.eval()
 
-    conf_matrix = np.zeros((2,2))
+    conf_matrix = np.zeros((2, 2))
     for i, (annotation, bag, targets) in enumerate(val_loader):
 
         bag = bag.permute(0, 2, 1, 3, 4)
@@ -117,17 +115,12 @@ def validation():
         loss = torch.nn.functional.binary_cross_entropy(output, targets.float())
         output = output.detach().cpu().numpy()
         targets = targets.detach().cpu().numpy()
-        conf_matrix+=metrics.confusion_matrix(targets, output>0.5)
+        conf_matrix += metrics.confusion_matrix(targets, output > 0.5)
     return conf_matrix
 
 
-
-
-
-
-
 def main():
-    metrics = {"train":[ [], [], [], [] ], "valid":[ [], [], [], [] ]}
+    metrics = {"train": [[], [], [], []], "valid": [[], [], [], []]}
     metric_names = ["accuracy", "recall", "precision", "f1"]
 
     for epoch in range(epochs):
@@ -137,48 +130,42 @@ def main():
         train_metrics = get_metrics(train_conf_matrix)
         val_metrics = get_metrics(val_conf_matrix)
 
-        print("train accuracy: {}, val acc: {}".format(train_metrics[0], val_metrics[0]))
+        print(
+            "train accuracy: {}, val acc: {}".format(train_metrics[0], val_metrics[0])
+        )
 
-        for i in range (len(metrics["train"])):
+        for i in range(len(metrics["train"])):
             metrics["train"][i].append(train_metrics[i])
             metrics["valid"][i].append(val_metrics[i])
 
-
         print("end of epoch")
-
-
-    
 
     if True:
         nbr_subplots = 4
-        fig, ax = plt.subplots(1,nbr_subplots,figsize=(20,10))
-        #fig.suptitle("Train-Validation accuracy", fontsize=16)
-    
+        fig, ax = plt.subplots(1, nbr_subplots, figsize=(20, 10))
+        # fig.suptitle("Train-Validation accuracy", fontsize=16)
 
-        for i in range (nbr_subplots):
+        for i in range(nbr_subplots):
             ax[i].set_title("Train-Validation {}".format(metric_names[i]))
-            ax[i].plot(metrics["train"][i], label='train')
-            ax[i].plot(metrics["valid"][i], label='validation')
-            ax[i].set_xlabel('num_epochs', fontsize=12)
+            ax[i].plot(metrics["train"][i], label="train")
+            ax[i].plot(metrics["valid"][i], label="validation")
+            ax[i].set_xlabel("num_epochs", fontsize=12)
             ax[i].set_ylabel(metric_names[i], fontsize=12)
-            ax[i].legend(loc='best')
+            ax[i].legend(loc="best")
 
-
-      
-
-        
-
-        plt.savefig('./results/plots/accuracy.png')
-        print('Plot saved to ' + './results/plots/accuracy.png')
+        plt.savefig("./results/plots/accuracy.png")
+        print("Plot saved to " + "./results/plots/accuracy.png")
     return 0
+
 
 def get_metrics(conf_matrix):
     tn, fp, fn, tp = conf_matrix.ravel()
 
-    accuracy = (tp+tn)/(tp+tn+fp+fn)
-    recall = tp/(tp+fn)
-    precision = tp/(tp+fp)
-    f1 = (2*recall*precision)/(recall+precision)
+    accuracy = (tp + tn) / (tp + tn + fp + fn)
+    recall = tp / (tp + fn)
+    precision = tp / (tp + fp)
+    f1 = (2 * recall * precision) / (recall + precision)
     return [accuracy, recall, precision, f1]
+
 
 main()
